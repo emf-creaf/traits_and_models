0e7210a36c7be4ceae63fcb4bd3311227fe1e411
Miquel De Cáceres <miquelcaceres@gmail.com>
2026-06-29 09:43:05 +0200

    revision Hoshika

--- R/trait_source_harmonization/Hoshika_et_al_2018.R
+++ R/trait_source_harmonization/Hoshika_et_al_2018.R
@@ -10,12 +10,14 @@ db <- readxl::read_excel(paste0(DB_path,"data-raw/raw_trait_data/Hoshika_et_al_2
 
 # Variable harmonization --------------------------------------------------
 db_var <- db |>
-  dplyr::select(Species, "gmax (mol m-2 s-1)") |>
+  dplyr::select(Species, "gmax (mol m-2 s-1)", "First author", "Year", "Journal") |>
   dplyr::rename(originalName = Species,
-                Value = "gmax (mol m-2 s-1)") |>
+                Value = "gmax (mol m-2 s-1)",
+                FirstAuthor = "First author") |>
   dplyr::mutate(Trait = "Gswmax",
                 Value = as.numeric(Value),
-                Units = "mol s-1 m-2")|>
+                Units = "mol s-1 m-2",
+                Level = "population")|>
   dplyr::mutate(originalName = stringr::str_replace(originalName, "Eleaegnus", "Elaeagnus"))|>
   dplyr::mutate(originalName = stringr::str_replace(originalName, " sp\\.", ""))|>
   dplyr::mutate(originalName = stringr::str_replace(originalName, "\\_", " "))|>
@@ -24,8 +26,10 @@ db_var <- db |>
   dplyr::mutate(Reference = "Hoshika et al. (2018). Global diurnal and nocturnal parameters of stomatal
 conductance in woody plants and major crops. Global Ecol. Biogeog. 27: 257-275",
                 DOI = "10.1111/geb.12681",
-                Priority = 3) |>
+                Priority = 3,
+                originalReference = paste0(FirstAuthor, " et al. (", Year, ") ", Journal)) |>
   dplyr::arrange(originalName) |>
+  dplyr::select(-FirstAuthor, -Year, -Journal)|>
   tibble::as_tibble()
 traits4models::check_harmonized_trait(db_var)
 # Taxonomic harmonization -----------------------------------------------
