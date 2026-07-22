d670423a56e8eca18162ca7ac9ecbe99b6ddf6c6
Miquel De Caceres <miquelcaceres@gmail.com>
2026-07-22 19:28:31 +0200

    trait analysis

--- R/trait_analysis/trait_info.R
+++ R/trait_analysis/trait_info.R
@@ -1,13 +1,21 @@
 trait_info<- function(harmonized_trait_path, WFO_file,
                                         trait_name) {
-  trait_name <- "Ks"
-  WFO_file <- "./data-raw/wfo_backbone/classification.csv"
-  species_means <- traits4models::taxon_trait_summary(harmonized_trait_path, traits =c(trait_name),
-                                                      summary_function = "n", verbose = FALSE, progress = FALSE) |>
-    dplyr::rename(originalName = acceptedName) |>
-    dplyr::filter(!is.na(.data[[trait_name]]), !is.infinite(.data[[trait_name]]))
+  if(trait_name=="Gsw_q99") {
+    species_means <- traits4models::taxon_trait_summary(harmonized_trait_path, traits ="Gsw",
+                                                        summary_function = "n", verbose = FALSE, progress = FALSE) |>
+      dplyr::rename(originalName = acceptedName) |>
+      dplyr::filter(!is.na(.data[["Gsw"]]), !is.infinite(.data[["Gsw"]])) |>
+      dplyr::mutate(Trait = trait_name)
+  } else {
+    species_means <- traits4models::taxon_trait_summary(harmonized_trait_path, traits =trait_name,
+                                                        summary_function = "n", verbose = FALSE, progress = FALSE) |>
+      dplyr::rename(originalName = acceptedName) |>
+      dplyr::filter(!is.na(.data[[trait_name]]), !is.infinite(.data[[trait_name]])) |>
+      dplyr::mutate(Trait = trait_name)
+  }
+  names(species_means)[2] <- "n_records"
   
-  species_cat <- traits4models::taxon_trait_summary(harmonized_trait_path, traits = c("LifeForm", "GrowthForm"),
+  species_cat <- traits4models::taxon_trait_summary(harmonized_trait_path, traits = c("LifeForm", "GrowthForm", "LeafSize", "LeafShape", "PhenologyType", "PhotosyntheticPathway", "WoodPorosityType"),
                                                     taxon_selection = species_means$originalName,
                                                     summary_function = "mode", verbose = FALSE, progress = FALSE)|>
     dplyr::rename(originalName = acceptedName)
@@ -22,4 +30,10 @@ trait_info<- function(harmonized_trait_path, WFO_file,
     dplyr::left_join(traits4models:::fam_data, by = c("family"="Family"))
   
   return(species_complete)
+}
+
+bind_rows_info <-function(...) {
+  df <- dplyr::bind_rows(...)
+  saveRDS(df, "data/trait_info.rds")
+  return(df)
 }
--- _targets_analysis.R
+++ _targets_analysis.R
@@ -27,28 +27,46 @@ tar_source("R/trait_analysis/trait_info.R")
 values <- tibble(
   trait = c("SLA", "WoodC", "LeafWidth",
             "LeafDensity", "WoodDensity", "FineRootDensity", "r635",
-            "conduit2sapwood", "Al2As", "Ks", "kleaf", 
+            "conduit2sapwood", "Al2As", "Ks", "kleaf", "kplant", 
             "SRL",
             "VCstem_P50", "VCleaf_P50", "VCroot_P50",
+            "VCstem_P12", "VCleaf_P12", "VCroot_P12",
+            "VCstem_P88", "VCleaf_P88", "VCroot_P88",
             "LeafPI0", "LeafEPS", "LeafAF",  "Ptlp",
             "StomatalDensity", "Gswmin", "Gswmax", "Gsw_q99", "Vmax", "Jmax",
             "Nleaf", "Nsapwood", "Nfineroot")
 )
 
+trait_info_targets <- tar_map(
+  values = values,
+  tar_target(name = info, trait_info(harmonized_trait_path, WFO_file, trait))
+)
+
 trait_analysis_targets <- tar_map(
   values = values,
-  tar_target(name = info, trait_info(harmonized_trait_path, WFO_file, trait)),
   tar_target(name = analysis, trait_taxonomic_partitioning(harmonized_trait_path, WFO_file, trait))
 )
-combined <- tar_combine(
-  combined_summaries,
+
+combined_info <- tar_combine(
+  combined_info_table,
+  trait_info_targets[["info"]],
+  command =bind_rows_info(!!!.x)
+)
+
+combined_partitioning <- tar_combine(
+  combined_partitioning_summaries,
   trait_analysis_targets[["analysis"]],
   command = summarize_partitioning(!!!.x)
 )
 
-list(trait_analysis_targets, 
-     combined,
-     tar_target(
-       name = trait_coordination_analysis,
-       command = trait_coordination(harmonized_trait_path)
-     ))
+coordination <- tar_target(
+  name = trait_coordination_analysis,
+  command = trait_coordination(harmonized_trait_path)
+)
+
+list(trait_info_targets, 
+     combined_info,
+     trait_analysis_targets, 
+     combined_partitioning,
+     coordination
+)
